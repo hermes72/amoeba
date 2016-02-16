d373ac44662c6643e01159f3ad8f0e09ec601201
hermes72 <sreyaschaithanya@gmail.com>
2016-02-16 22:43:32 +0530

    modified the urls in sudopodia routing for some inconsistent routing patterns

--- sudopodia/urls.py
+++ sudopodia/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     url(r'^api$', views.api),
-    url(r'^/', views.index),
-    url(r'^register/', views.registerevent)
+    url(r'^api/$', views.api),
+    url(r'^register/$', views.registerevent),
+    url(r'^register$', views.registerevent),
+    url(r'^$', views.index)
 ]
